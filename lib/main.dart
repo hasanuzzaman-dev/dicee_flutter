dd4031a5e47d2e7dcc41f8c9c13ca664b92b3125
Hasan <hasanuzzaman.dev@gmail.com>
2021-06-09 18:28:27 +0600

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,33 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      backgroundColor: Colors.red,
-      appBar: AppBar(
-        title: Text('Dice'),
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
         backgroundColor: Colors.red,
-        elevation: 0.0,
+        appBar: AppBar(
+          title: Text('Dice'),
+          backgroundColor: Colors.red,
+          //elevation: 0.0,
+        ),
+        body: DicePage(),
       ),
-      body: DicePage(),
     ),
-  ));
+  );
 }
 
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Row(
+      children: [
+        Expanded(
+          child: Image(
+            image: AssetImage('assets/images/dice1.png'),
+          ),
+        )
+      ],
+    );
   }
 }
